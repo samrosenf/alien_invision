efd6780f7ede15e39b30213fa036c2826945b410
Sam Rosenfeld <samrosenf@gmail.com>
2021-10-31 20:22:04 +0200

    added ships lives to the score on the screen

--- Scoreboard.py
+++ Scoreboard.py
@@ -1,4 +1,7 @@
 import pygame.font
+from pygame.sprite import Group
+
+from ship import Ship
 
 
 class Scoreboard:
@@ -6,6 +9,7 @@ class Scoreboard:
 
     def __init__(self, ai_game):
         """Initialize scorekeeping attributes."""
+        self.ai_game = ai_game
         self.screen = ai_game.screen
         self.screen_rect = self.screen.get_rect()
         self.settings = ai_game.settings
@@ -19,6 +23,7 @@ class Scoreboard:
         self.prep_score()
         self.prep_high_score()
         self.prep_level()
+        self.prep_ships()
 
     def prep_score(self):
         """Turn the score into a rendered image."""
@@ -60,9 +65,19 @@ class Scoreboard:
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
         self.screen.blit(self.level_image, self.level_rect)
+        self.ships.draw(self.screen)
 
     def check_high_score(self):
         """Chech to see if there's a new high score."""
         if self.stats.score > self.stats.high_score:
             self.stats.high_score = self.stats.score
             self.prep_high_score()
+
+    def prep_ships(self):
+        """Show how mnay ships are left."""
+        self.ships = Group()
+        for ship_number in range(self.stats.ships_left):
+            ship = Ship(self.ai_game)
+            ship.rect.x = 10 + ship_number * ship.rect.width
+            ship.rect.y = 10
+            self.ships.add(ship)
--- alien_invasion.py
+++ alien_invasion.py
@@ -96,6 +96,7 @@ class AlienInvasion:
         if self.stats.ships_left > 1:        
             # Decrement ships left.
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
 
             # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
@@ -173,16 +174,17 @@ class AlienInvasion:
         self.stats.game_active = True
         self.sb.prep_score()
         self.sb.prep_level()
+        self.sb.prep_ships()
 
-            # Get rid of any remaining aliens and bullets.
+        # Get rid of any remaining aliens and bullets.
         self.aliens.empty()
         self.bullets.empty()
 
-            # Create a new fleet and center the ship.
+        # Create a new fleet and center the ship.
         self._create_fleet()
         self.ship.center_ship()
 
-            # Hide the mouse cursor.
+        # Hide the mouse cursor.
         pygame.mouse.set_visible(False)
 
     def _check_keyup_events(self, event):
