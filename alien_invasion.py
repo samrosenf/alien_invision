3710f2ac81c2dc3b0c6e8394694461db5c30bc13
Sam Rosenfeld <samrosenf@gmail.com>
2021-10-31 18:09:59 +0200

    change speed as leveling up

--- alien_invasion.py
+++ alien_invasion.py
@@ -68,6 +68,7 @@ class AlienInvasion:
             # Destory existing bullets and create new fleet.
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
 
     def _check_aliens_bottom(self):
         """Check if any aliens have reached the bottom of the screen."""
@@ -147,6 +148,8 @@ class AlienInvasion:
         """Start a new game when the player clicks Play."""
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
+            # Reset the game settings.
+            self.settings.initialize_dynamic_settings()
             self._start_game()
 
     def _start_game(self):
