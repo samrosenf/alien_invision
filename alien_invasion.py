e5389e55bc38bbf12c1e70b699026b25c463bee8
Sam Rosenfeld <samrosenf@gmail.com>
2021-11-01 12:26:06 +0200

    add up and down movement

--- alien_invasion.py
+++ alien_invasion.py
@@ -199,12 +199,20 @@ class AlienInvasion:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
+        elif event.key == pygame.K_UP:
+            self.ship.moving_up = False
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = False
 
     def _check_keydown_events(self, event):
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_UP:
+            self.ship.moving_up = True
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = True
         elif event.key == pygame.K_q:
             self._save_high_score_and_exit()
         elif event.key == pygame.K_SPACE:
